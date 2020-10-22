bf8ed68a5968bd27c17b740357ed476bda751256
Kevin Gamez <ks.gamez@uniandes.edu.co>
2020-10-21 19:47:48 -0500

    Update

--- scripts/dataCourses.js
+++ scripts/dataCourses.js
@@ -5,7 +5,7 @@ export var dataCourses = [
     new Course("Algebra Lineal 2", "Camilo Sanabria", 3),
     new Course("Ecuaciones Diferenciales", "Mikhail Malakhaltsev", 3),
     new Course("Ingles 7", "Edinson Romero", 0),
-    new Course("Ingles 7", "Alejando Mcneil", 0),
+    new Course("Ingles 8", "Alejando Mcneil", 0),
     new Course("Infraestructura Computacional", "Sandra Rueda", 3),
     new Course("Sistemas Transaccionales", "German Bravo", 3),
     new Course("TI en las Organizaciones", "Cindy Hernandez", 3)
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { dataCourses } from "./dataCourses";
+import { dataCourses } from './dataCourses.js';
 var coursesTbody = document.getElementById('courses');
 var btnfilterByName = document.getElementById("button-filterByName");
 var inputSearchBox = document.getElementById("search-box");
@@ -7,16 +7,12 @@ btnfilterByName.onclick = function () { return applyFilterByName(); };
 renderCoursesInTable(dataCourses);
 totalCreditElm.innerHTML = "" + getTotalCredits(dataCourses);
 function renderCoursesInTable(courses) {
-    courses.forEach(function (c) {
+    console.log('Desplegando cursos');
+    courses.forEach(function (course) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>" + c.name + "</td>\n                           <td>" + c.professor + "</td>\n                           <td>" + c.credits + "</td>";
+        trElement.innerHTML = "<td>" + course.name + "</td>\n                           <td>" + course.professor + "</td>\n                           <td>" + course.credits + "</td>";
         coursesTbody.appendChild(trElement);
     });
-}
-function getTotalCredits(courses) {
-    var totalCredits = 0;
-    courses.forEach(function (course) { return totalCredits = totalCredits + course.credits; });
-    return totalCredits;
 }
 function applyFilterByName() {
     var text = inputSearchBox.value;
@@ -29,6 +25,11 @@ function searchCourseByName(nameKey, courses) {
     return nameKey === '' ? dataCourses : courses.filter(function (c) {
         return c.name.match(nameKey);
     });
+}
+function getTotalCredits(courses) {
+    var totalCredits = 0;
+    courses.forEach(function (course) { return totalCredits = totalCredits + course.credits; });
+    return totalCredits;
 }
 function clearCoursesInTable() {
     while (coursesTbody.hasChildNodes()) {
